dd09ad59e48db4948f8356cb9e946259a123a0c7
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-10-28 16:20:41 -0300

    fix: code format

--- apps/torus-cache/client_example.ts
+++ apps/torus-cache/client_example.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { STAKE_DATA_SCHEMA } from "@torus-ts/types";
 import fetch from "node-fetch";
 import SuperJSON from "superjson";
+
+import { STAKE_DATA_SCHEMA } from "@torus-ts/types";
 
 // TODO: use `superjson` instead of `json-bigint`
 
 const result = await fetch("http://localhost:3000/api/stake-out");
-const data = await result.text()
+const data = await result.text();
 
 const parsedData = SuperJSON.parse(data);
-const stakeOutData = STAKE_DATA_SCHEMA.parse(parsedData); 
+const stakeOutData = STAKE_DATA_SCHEMA.parse(parsedData);
 
 console.log(stakeOutData.perAddr);
 console.log("total: ", stakeOutData.total);
