a5a378ab4ef778c7e17fb713f44cf822c09d780d
Kelvin Steiner <me@steinerkelvin.dev>
2024-11-13 10:03:35 -0300

    fix: typings
    @vinicius-sacramento trolling u.u

--- apps/torus-governance/src/app/components/mobile-header-content.tsx
+++ apps/torus-governance/src/app/components/mobile-header-content.tsx
@@ -2,15 +2,15 @@
 
 import React from "react";
 
-import { BalanceSection } from "./balance-section";
+// import { BalanceSection } from "./balance-section";
 import { CreateModal } from "./modal";
 
 export const MobileHeaderContent = () => {
   return (
     <>
-      <div className="flex w-full flex-col text-green-500">
+      {/* <div className="flex w-full flex-col text-green-500">
         <BalanceSection />
-      </div>
+      </div> */}
 
       <div className="flex w-full flex-col gap-4 border-b border-white/20 p-4 text-green-500">
         <CreateModal />
