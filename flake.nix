1fb9906c2cd181bed26c64c9fb0fe67ac3f54f10
Kelvin Steiner <me@steinerkelvin.dev>
2024-11-14 08:09:17 -0300

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
