ae0d5762d985121ea6cdcb9e219d2e8789ca5144
EdSDR <ed.sdr@outlook.com>
2024-12-17 16:47:33 -0300

    refactor: remove unused prettier plugin and update configuration

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -3,14 +3,10 @@ import { fileURLToPath } from "url";
 
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
-/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
+  plugins: ["prettier-plugin-tailwindcss"],
   tailwindConfig: fileURLToPath(
     new URL("../../tooling/tailwind/web.ts", import.meta.url),
   ),
