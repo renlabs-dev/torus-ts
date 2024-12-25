3fd384904e1f8cd9f6c16a4f1dfac86f5a667075
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-12-24 21:01:16 -0300

    fix: wallet selection on bridge

--- packages/ui/src/components/no-wallet-extension-display.tsx
+++ packages/ui/src/components/no-wallet-extension-display.tsx
@@ -1,10 +1,11 @@
 export function NoWalletExtensionDisplay() {
   return (
     <div className="flex h-full flex-col gap-3 p-3 text-sm">
-      <div className="flex flex-col gap-4">
+      <div className="flex max-w-80 flex-col gap-4">
         <b className="text-red-400">No wallet found.</b>
         <p className="text-foreground">
-          To use this app you must enable your existing wallet.
+          To use this app you must enable your existing wallet on your extension
+          settings.
         </p>
       </div>
       <div className="flex flex-col gap-1">
