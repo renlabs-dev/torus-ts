59ed0cc9eb3f7c89b053a60a5875e288c8a74668
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-12-25 21:27:22 -0300

    feat: improvements on the new toast content handler

--- packages/torus-provider/src/_components/toast-content-handler.tsx
+++ packages/torus-provider/src/_components/toast-content-handler.tsx
@@ -1,22 +1,31 @@
+import type { CSSProperties } from "react";
+
+const divStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+  color: "hsl(240 5% 64.9%)",
+};
+
+const linkStyle: CSSProperties = {
+  color: "white",
+  textDecoration: "underline",
+  fontSize: "0.875rem",
+  cursor: "pointer",
+};
+
+const POLKADOT_JS_EXPLORER_URL = {
+  dev: "https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Ftestnet.api.communeai.net#/explorer/query",
+  prod: "https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fapi.communeai.net#/explorer/query",
+};
+
 export const renderWaitingForValidation = (hash: string) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "0.5rem",
-      color: "hsl(240 5% 64.9%)",
-    }}
-  >
+  <div style={divStyle}>
     <p>Processing transaction block.</p>
     <a
-      style={{
-        color: "white",
-        textDecoration: "underline",
-        fontSize: "0.875rem",
-        lineHeight: "1.25rem",
-      }}
+      style={linkStyle}
       target="_blank"
-      href={`https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Ftestnet.api.communeai.net#/explorer/query/${hash}`}
+      href={`${POLKADOT_JS_EXPLORER_URL.dev}/${hash}`}
     >
       View on block explorer
     </a>
@@ -27,22 +36,10 @@ export const renderSuccessfulyFinalized = (
   transactionType: string,
   hash: string,
 ) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "0.5rem",
-      color: "hsl(240 5% 64.9%)",
-    }}
-  >
+  <div style={divStyle}>
     <p>{transactionType} was finalized successfully.</p>
     <a
-      style={{
-        color: "white",
-        textDecoration: "underline",
-        fontSize: "0.875rem",
-        lineHeight: "1.25rem",
-      }}
+      style={linkStyle}
       target="_blank"
       href={`https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Ftestnet.api.communeai.net#/explorer/query/${hash}`}
     >
@@ -52,22 +49,10 @@ export const renderSuccessfulyFinalized = (
 );
 
 export const renderFinalizedWithError = (msg: string, hash: string) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "0.5rem",
-      color: "hsl(240 5% 64.9%)",
-    }}
-  >
+  <div style={divStyle}>
     <p>{msg}</p>
     <a
-      style={{
-        color: "white",
-        textDecoration: "underline",
-        fontSize: "0.875rem",
-        lineHeight: "1.25rem",
-      }}
+      style={linkStyle}
       target="_blank"
       href={`https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Ftestnet.api.communeai.net#/explorer/query/${hash}`}
     >
