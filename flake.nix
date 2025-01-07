82da4199cc74708e03d2ad1fbe9d6fbd5e361051
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-07 11:15:54 -0300

    chore: add native dependency to build `usb` module for Solana wallet for hyperlane
    + bump flake

--- flake.nix
+++ flake.nix
@@ -6,21 +6,29 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nativeBuildInputs = [
+          # systemd provides libudev for `npm:usb` because of Solana adapter on Hyperlane
+          pkgs.systemd
+        ];
+        buildInputs = [
+          pkgs.nodejs_20
+          pkgs.pnpm
+        ];
+        shellPkgs = [
+          # # Git LFS
+          # pkgs.git-lfs
+          # Run project-specific commands
+          pkgs.just
+          # Run Github actions locally
+          pkgs.act
+        ];
+      in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            # Git
-            pkgs.git-lfs
-            # Run project-specific commands
-            pkgs.just
-            # Run Github actions locally
-            pkgs.act
-            # Node.js
-            pkgs.nodejs_20
-            pkgs.pnpm
-            pkgs.biome
-          ];
+          inherit nativeBuildInputs buildInputs;
+          packages = shellPkgs;
         };
       }
     );
