6d511718f75158e0fa09eba1ae0582ab0cf29806
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-07 17:40:21 -0300

    feat: add evm to SS58 address conversion

--- packages/subspace/src/index.ts
+++ packages/subspace/src/index.ts
@@ -1,13 +1,16 @@
-// == Modules ==
-export * from "./modules/_common";
-export * from "./modules/subspace";
-export * from "./modules/governance";
-
 // === Address ===
 export * from "./address";
 
 // == RPC ==
 export * from "./rpc";
+
+// == Modules ==
+export * from "./modules/_common";
+export * from "./modules/subspace";
+export * from "./modules/governance";
+
+// == EVM ==
+export * from "./evm";
 
 // == Cached Queries ==
 export * from "./cached-queries";
