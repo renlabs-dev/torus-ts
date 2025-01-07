8f3ee876cd2bb2034c7a576ee1ae7c72aefe2583
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-07 18:30:23 -0300

    fix: evm to SS58 address conversion

--- packages/subspace/src/evm.ts
+++ packages/subspace/src/evm.ts
@@ -1,18 +1,24 @@
-import { hexToU8a } from "@polkadot/util";
+import { hexToU8a, stringToU8a } from "@polkadot/util";
 import { blake2AsU8a, encodeAddress } from "@polkadot/util-crypto";
 
 import type { SS58Address } from "./address";
+import { assert } from "tsafe";
+
+const ADDRESS_FORMAT = 42;
 
 /**
  * Converts an Ethereum H160 address to a Substrate SS58 address public key.
  * @param ethAddress - The H160 Ethereum address as a hex string.
  * @return The bytes array containing the Substrate public key.
  */
-export function convertH160ToSS58(ethAddress: string, ss58_format = 42) {
-  const prefix = "evm:";
-  const prefixBytes = new TextEncoder().encode(prefix);
+export function convertH160ToSS58(
+  ethAddress: string,
+  ss58_format = ADDRESS_FORMAT,
+) {
+  assert(ethAddress.startsWith("0x"), "EVM address must start with 0x");
 
-  const addressBytes = hexToU8a(ensurePrefix(prefix, ethAddress));
+  const prefixBytes = stringToU8a("evm:");
+  const addressBytes = hexToU8a(ethAddress);
 
   // Concatenate prefix and Ethereum address
   const combined = new Uint8Array(prefixBytes.length + addressBytes.length);
@@ -26,6 +32,3 @@ export function convertH160ToSS58(ethAddress: string, ss58_format = 42) {
   const ss58Address = encodeAddress(hash, ss58_format);
   return ss58Address as SS58Address;
 }
-
-const ensurePrefix = (prefix: string, txt: string) =>
-  txt.startsWith(prefix) ? txt : `${prefix}${txt}`;
