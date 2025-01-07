5ab85e8c00d47ed1e291a7e226b9685581dd9c85
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-07 20:33:45 -0300

    feat: implement Torus EVM withdraw

--- packages/subspace/src/evm.ts
+++ packages/subspace/src/evm.ts
@@ -1,8 +1,15 @@
+import { assert } from "tsafe";
+import type { AbiFunction, Chain, WalletClient } from "viem";
+import { encodeAbiParameters } from "viem";
+
 import { hexToU8a, stringToU8a } from "@polkadot/util";
-import { blake2AsU8a, encodeAddress } from "@polkadot/util-crypto";
+import {
+  blake2AsU8a,
+  decodeAddress,
+  encodeAddress,
+} from "@polkadot/util-crypto";
 
 import type { SS58Address } from "./address";
-import { assert } from "tsafe";
 
 const ADDRESS_FORMAT = 42;
 
@@ -31,4 +38,54 @@ export function convertH160ToSS58(
   // Convert the hash to SS58 format
   const ss58Address = encodeAddress(hash, ss58_format);
   return ss58Address as SS58Address;
+}
+
+// == Withdraw ==
+
+// Precompile contract address
+const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000800";
+
+// BalanceTransfer ABI
+const balance_transfer_abi: AbiFunction = {
+  inputs: [
+    {
+      internalType: "bytes32",
+      name: "data",
+      type: "bytes32",
+    },
+  ],
+  name: "transfer",
+  outputs: [],
+  stateMutability: "payable",
+  type: "function",
+};
+
+export async function withdrawFromTorusEvm(
+  walletClient: WalletClient,
+  chain: Chain,
+  destination: SS58Address,
+  value: bigint,
+) {
+  if (!walletClient.account) {
+    throw new Error("Wallet client account is undefined");
+  }
+  const pubk = decodeAddress(destination);
+  const pubk_hex = Buffer.from(pubk).toString("hex").padStart(64, "0");
+  // console.debug(`pubk_hex = ${pubk_hex}`);
+
+  // Prepare transaction data
+  const encodedData = encodeAbiParameters(balance_transfer_abi.inputs, [
+    `0x${pubk_hex}`,
+  ]);
+
+  // Send transaction using walletClient
+  const txHash = await walletClient.sendTransaction({
+    account: walletClient.account,
+    to: CONTRACT_ADDRESS,
+    data: encodedData,
+    value,
+    chain,
+  });
+
+  return txHash;
 }
