2fbf4de7954e0825e9fbcd78ebdabda59f78a517
EdSDR <ed.sdr@outlook.com>
2025-01-09 17:03:21 -0300

    feat: transfer details component test

---
+++ apps/torus-bridge/src/app/_components/transfer-token/_components/side-bar.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from "react";
+
+import { useStore } from "~/utils/store";
+import type { TransferContext } from "~/utils/types";
+import { TransfersDetailsModal } from "./transfer-details";
+
+export function SideBarMenu() {
+  const didMountRef = useRef(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedTransfer, setSelectedTransfer] = useState<
+    TransferContext | undefined | null
+  >(null);
+
+  const { transfers, transferLoading } = useStore((s) => ({
+    transfers: s.transfers,
+    resetTransfers: s.resetTransfers,
+    transferLoading: s.transferLoading,
+  }));
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+    } else if (transferLoading) {
+      setSelectedTransfer(transfers[transfers.length - 1]);
+      setIsModalOpen(true);
+    }
+  }, [transfers, transferLoading]);
+
+  return (
+    <>
+      {selectedTransfer && (
+        <TransfersDetailsModal
+          isOpen={isModalOpen}
+          onClose={() => {
+            setIsModalOpen(false);
+            setSelectedTransfer(null);
+          }}
+          transfer={selectedTransfer}
+        />
+      )}
+    </>
+  );
+}
