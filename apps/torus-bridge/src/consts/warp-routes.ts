ca90ad97921b2a1d35b22c790681f7110c8daa42
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-09 12:24:37 -0300

    fix: make `warp-route.ts` consistent with `warp-routes.yaml`

--- apps/torus-bridge/src/consts/warp-routes.ts
+++ apps/torus-bridge/src/consts/warp-routes.ts
@@ -11,13 +11,13 @@ export const WarpRoutesTs: WarpCoreConfig = {
       standard: TokenStandard.EvmHypNative,
       decimals: 18,
       symbol: "TORUS",
-      name: "TorusTestnet",
+      name: "Torus Testnet",
       addressOrDenom: "0x677723050085BFE889726B58a14418a92cBF7f36",
-      logoURI: "/api/placeholder/400/300",
+      logoURI: "/TODO",
       connections: [
         {
           token:
-            "ethereum|torustestnet|0x677723050085BFE889726B58a14418a92cBF7f36",
+            "ethereum|basesepolia|0x1c87f74D6E39F567cB1aac0e091Dc433a702ABd7",
         },
       ],
     },
@@ -25,14 +25,14 @@ export const WarpRoutesTs: WarpCoreConfig = {
       chainName: "basesepolia",
       standard: TokenStandard.EvmHypSynthetic,
       decimals: 18,
-      symbol: "ETH",
-      name: "Ether",
+      symbol: "TORUS",
+      name: "Torus Testnet Wrapped",
       addressOrDenom: "0x1c87f74D6E39F567cB1aac0e091Dc433a702ABd7",
-      logoURI: "/api/placeholder/400/300",
+      logoURI: "/TODO",
       connections: [
         {
           token:
-            "ethereum|basesepolia|0x1c87f74D6E39F567cB1aac0e091Dc433a702ABd7",
+            "ethereum|torustestnet|0x677723050085BFE889726B58a14418a92cBF7f36",
         },
       ],
     },
