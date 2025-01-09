45730a41f6caba3465f96dedc811f76dd8772feb
EdSDR <ed.sdr@outlook.com>
2025-01-09 01:57:49 -0300

    feat: rename app to "Torus Base Bridge" and refactor layout to use AppContextProvider for improved context management

--- apps/torus-bridge/src/app/layout.tsx
+++ apps/torus-bridge/src/app/layout.tsx
@@ -1,22 +1,17 @@
-"use client";
-
 import "../styles/globals.css";
-import { useIsSsr } from "@hyperlane-xyz/widgets";
 
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
+import { Layout } from "@torus-ts/ui";
 import { Fira_Mono as FiraMono } from "next/font/google";
-import { CosmosWalletProvider } from "~/context/cosmos-wallet-provider";
-import { EvmWalletProvider } from "~/context/evm-wallet-provider";
-import { WarpContextInitGateProvider } from "~/context/warp-context-init-gate-provider";
+import { AppContextProvider } from "~/context/app-context-provider";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastProvider } from "@torus-ts/toast-provider";
-import { Layout, Loading } from "@torus-ts/ui";
-
-import { SolanaWalletProvider } from "~/context/solana-wallet-provider";
-import { TorusProvider } from "@torus-ts/torus-provider";
-import { env } from "~/env";
-import { WalletHeader } from "./_components/shared/wallet-header";
+export const metadata: Metadata = {
+  robots: "all",
+  title: "Torus Base Bridge",
+  icons: [{ rel: "icon", url: "favicon.ico" }],
+  description:
+    "Simple, secure, and easy-to-use wallet for the torus ecosystem.",
+};
 
 export const firaMono = FiraMono({
   subsets: ["latin"],
@@ -24,53 +19,14 @@ export const firaMono = FiraMono({
   weight: "400",
 });
 
-const reactQueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  // // Disable app SSR for now as it's not needed and
-  // // complicates wallet and graphql integrations
-  const isSsr = useIsSsr();
-  if (isSsr) {
-    return (
-      <Layout font={firaMono}>
-        <div className="min-w-screen flex min-h-screen items-center justify-center">
-          <Loading /> Loading Bridge...
-        </div>
-      </Layout>
-    );
-  }
-
   return (
     <Layout font={firaMono}>
-      <ToastProvider>
-        <QueryClientProvider client={reactQueryClient}>
-          <TorusProvider
-            wsEndpoint={env.NEXT_PUBLIC_TORUS_RPC_URL}
-            torusCacheUrl={env.NEXT_PUBLIC_TORUS_CACHE_URL}
-          >
-            <WarpContextInitGateProvider>
-              <EvmWalletProvider>
-                <SolanaWalletProvider>
-                  <CosmosWalletProvider>
-                    <WalletHeader />
-                    {children}
-                  </CosmosWalletProvider>
-                </SolanaWalletProvider>
-              </EvmWalletProvider>
-            </WarpContextInitGateProvider>
-          </TorusProvider>
-        </QueryClientProvider>
-      </ToastProvider>
+      <AppContextProvider>{children}</AppContextProvider>
     </Layout>
   );
 }
