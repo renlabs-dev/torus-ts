433b5545b6fd820ad5ca92e9cfa60e550e31d989
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-08 21:45:06 -0300

    fix: update chainId and domainId to 21001 for torustestnet in chains.ts and chains.yaml

--- apps/torus-bridge/src/consts/chains.ts
+++ apps/torus-bridge/src/consts/chains.ts
@@ -5,11 +5,11 @@ import { ProtocolType } from "@hyperlane-xyz/utils";
 export const chainsTS: ChainMap<ChainMetadata & { mailbox?: Address }> = {
   torustestnet: {
     protocol: ProtocolType.Ethereum,
-    chainId: 21000,
-    domainId: 21000,
+    chainId: 21001,
+    domainId: 21001,
     name: "torustestnet",
     displayName: "TorusTestnet",
-    nativeToken: { name: "tours", symbol: "TORUS", decimals: 18 },
+    nativeToken: { name: "torus", symbol: "TORUS", decimals: 18 },
     // eslint-disable-next-line no-restricted-properties
     rpcUrls: [{ http: String(process.env.NEXT_PUBLIC_TORUS_RPC_HTTPS_URL) }],
     blocks: {
