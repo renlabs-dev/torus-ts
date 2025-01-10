0eba9a9a051624e43cfb520d8a4676f42c40d887
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-10 15:50:27 -0300

    feat: implement dynamic chain values for Torus and Base environments in wallet balance component
    based on `NEXT_PUBLIC_CHAIN_ENV` env var

---
+++ apps/torus-bridge/src/config.ts
@@ -0,0 +1,51 @@
+export interface ChainValues {
+  name: string;
+  displayName: string;
+  chainId: number;
+}
+
+export type ChainEnv = "mainnet" | "testnet";
+
+export const evmChainValues = {
+  torus: {
+    mainnet: {
+      name: "torus",
+      displayName: "Torus EVM",
+      chainId: 21000,
+    },
+    testnet: {
+      name: "torustestnet",
+      displayName: "Torus Testnet EVM",
+      chainId: 21001,
+    },
+  },
+  base: {
+    mainnet: {
+      name: "base",
+      displayName: "Base",
+      chainId: 8453,
+    },
+    testnet: {
+      name: "base",
+      displayName: "Base",
+      chainId: 84532,
+    },
+  },
+} satisfies Record<string, Record<ChainEnv, ChainValues>>;
+
+/**
+ * Returns a function that retrieves chain values for a given environment.
+ *
+ * @param env - The environment for which to retrieve chain values.
+ * @returns A function that takes a chain name and returns the corresponding chain values for the specified environment.
+ *
+ * @throws Will throw an error if there are no chain values for the specified chain name.
+ */
+export const getChainValuesOnEnv =
+  (env: ChainEnv) =>
+  <Name extends keyof typeof evmChainValues>(chainName: Name) => {
+    const chainVals = evmChainValues[chainName];
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (chainVals == null) throw new Error(`No chain values for ${chainName}`);
+    return chainVals[env];
+  };
--- apps/torus-bridge/src/env.ts
+++ apps/torus-bridge/src/env.ts
@@ -17,16 +17,20 @@ export const env = createEnv({
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
    */
   client: {
+    NEXT_PUBLIC_CHAIN_ENV: z.enum(["mainnet", "testnet"]).default("testnet"),
     NEXT_PUBLIC_TORUS_RPC_URL: z.string().url(),
     NEXT_PUBLIC_TORUS_CACHE_URL: z.string().url(),
+    // NEXT_PUBLIC_TORUS_RPC_HTTPS_URL: string().url(),
   },
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_CHAIN_ENV: process.env.NEXT_PUBLIC_CHAIN_ENV,
     NEXT_PUBLIC_TORUS_RPC_URL: process.env.NEXT_PUBLIC_TORUS_RPC_URL,
     NEXT_PUBLIC_TORUS_CACHE_URL: process.env.NEXT_PUBLIC_TORUS_CACHE_URL,
+    // NEXT_PUBLIC_TORUS_RPC_HTTPS_URL: process.env.NEXT_PUBLIC_TORUS_RPC_HTTPS_URL,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
