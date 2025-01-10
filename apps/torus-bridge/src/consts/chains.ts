9d333a5b2664ce56190c4640ecc06c9514ae0058
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-10 14:50:53 -0300

    chore: small adjustments on chain definitions

--- apps/torus-bridge/src/consts/chains.ts
+++ apps/torus-bridge/src/consts/chains.ts
@@ -5,20 +5,18 @@ import { ExplorerFamily } from "@hyperlane-xyz/sdk";
 import type { Address } from "@hyperlane-xyz/utils";
 import { ProtocolType } from "@hyperlane-xyz/utils";
 
-const RPC_URL_SCHEMA = z.string().url("Must be a valid URL");
-
 export const chainsTS: ChainMap<ChainMetadata & { mailbox?: Address }> = {
   torustestnet: {
     protocol: ProtocolType.Ethereum,
     chainId: 21001,
     domainId: 21001,
+    isTestnet: true,
     name: "torustestnet",
     displayName: "Torus Testnet",
-    nativeToken: { name: "torus", symbol: "TORUS", decimals: 18 },
+    nativeToken: { name: "torustestnet", symbol: "TORUS", decimals: 18 },
     rpcUrls: [
       {
-        // eslint-disable-next-line no-restricted-properties
-        http: RPC_URL_SCHEMA.parse(process.env.NEXT_PUBLIC_TORUS_RPC_HTTPS_URL),
+        http: "https://api.testnet.torus.network",
       },
     ],
     blocks: {
@@ -28,11 +26,10 @@ export const chainsTS: ChainMap<ChainMetadata & { mailbox?: Address }> = {
     },
     blockExplorers: [
       {
-        name: "Blockscout",
-        family: ExplorerFamily.Etherscan,
+        name: "Torus Testnet Blockscout",
+        family: ExplorerFamily.Blockscout,
         url: "https://blockscout.testnet.torus.network",
         apiUrl: "https://api.blockscout.testnet.torus.network/api",
-        // apiKey: "https://api.blockscout.testnet.torus.network/api",
       },
     ],
     logoURI: "/logo.svg",
