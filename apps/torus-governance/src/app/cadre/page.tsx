09a6ff51f3091f95f802e2bd9a50bd3af05cf9dd
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-01-11 16:26:40 -0300

    refactor: remove CadreSectionContainer and adjust layout in CadrePage

--- apps/torus-governance/src/app/cadre/page.tsx
+++ apps/torus-governance/src/app/cadre/page.tsx
@@ -1,32 +1,38 @@
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { CadreSectionContainer, CadreMembersList, CadreRequestsList } from "./_components";
+import { CadreMembersList, CadreRequestsList } from "./_components";
+import { Button } from "@torus-ts/ui";
 
 export default function CadrePage() {
   return (
-    <div className="mx-auto flex max-w-screen-2xl flex-col px-4">
-      <Link
-        className="my-6 ml-2 flex w-fit animate-fade-down items-center justify-center gap-2 border border-white/20 bg-[#898989]/5 px-5 py-3 text-gray-400 backdrop-blur-md transition duration-200 hover:border-green-500 hover:bg-green-500/20 hover:text-green-500"
-        href="/"
+    <div className="mx-auto flex w-full max-w-screen-xl flex-col py-12">
+      <Button
+        asChild
+        variant="link"
+        className="mb-6 flex w-fit items-center gap-1.5 p-0"
       >
-        <ArrowLeft className="h-6 text-green-500" />
-        Go Back to Proposals List
-      </Link>
+        <Link
+          href="/?view=cadre"
+          className="flex animate-fade-left items-center text-white transition duration-200"
+        >
+          <ArrowLeft className="h-5 w-5" />
+          Go back to agents list
+        </Link>
+      </Button>
 
-      <CadreSectionContainer>
-        <div className="mb-4 w-full border-b border-gray-500 border-white/20 pb-1 text-gray-400">
-          <h2 className="text-start text-lg font-semibold text-gray-300">Curator DAO Join Requests</h2>
-        </div>
+      <div>
+        <h2 className="text-start text-xl font-semibold text-gray-300">
+          Curator DAO Join Requests
+        </h2>
         <CadreRequestsList />
-      </CadreSectionContainer >
+      </div>
 
-      <CadreSectionContainer>
-        <div className="mb-4 w-full border-b border-gray-500 border-white/20 pb-1 text-gray-400">
-          <h2 className="text-start text-lg font-semibold text-gray-300">Curator DAO Members</h2>
-        </div>
-        <CadreMembersList />
-      </CadreSectionContainer>
+      <div className="mb-4 w-full border-b border-gray-500 border-white/20 pb-1 text-gray-400">
+        <h2 className="text-start text-lg font-semibold text-gray-300">
+          Curator DAO Members Discord Id's
+        </h2>
+      </div>
+      <CadreMembersList />
     </div>
-  )
+  );
 }
-
