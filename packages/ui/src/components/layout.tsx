58389f65f154579ccd5d8a03f2149332e617cde5
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-01-14 19:12:49 -0300

    fix: scrollbar

--- packages/ui/src/components/layout.tsx
+++ packages/ui/src/components/layout.tsx
@@ -27,7 +27,7 @@ export function Layout({
       <body
         className={cn(
           font.className,
-          `overscroll-none bg-[#020202] text-white`,
+          `overflow-auto bg-[#020202] text-white`,
           className,
         )}
       >
