ca95439669b18d08cff277d90b3a5cb058c545d0
Gabriel Cipriano <gabriel@cipriano.dev>
2025-01-16 00:11:02 +0100

    feat: use envScript for validation and runtime

--- apps/torus-wallet/src/env.ts
+++ apps/torus-wallet/src/env.ts
@@ -1,33 +1,20 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+import { buildZodEnvScript } from "@torus-ts/env-validation";
 
-export const env = createEnv({
-  shared: {
-    NODE_ENV: z
-      .enum(["development", "production", "test"])
-      .default("development"),
-  },
-  /**
-   * Specify your server-side environment variables schema here.
-   * This way you can ensure the app isn't built with invalid env vars.
-   */
-  server: {},
+const NodeEnvSchema = z.enum(["development", "production", "test"])
+
+buildZodEnvScript
+const EnvSchema = z.object({
+  NODE_ENV: NodeEnvSchema.default("development"),
   /**
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
    */
-  client: {
-    NEXT_PUBLIC_TORUS_RPC_URL: z.string().url(),
-    NEXT_PUBLIC_TORUS_CACHE_URL: z.string().url(),
-  },
-  /**
-   * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
-   */
-  experimental__runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    NEXT_PUBLIC_TORUS_RPC_URL: process.env.NEXT_PUBLIC_TORUS_RPC_URL,
-    NEXT_PUBLIC_TORUS_CACHE_URL: process.env.NEXT_PUBLIC_TORUS_CACHE_URL,
-  },
-  skipValidation:
-    !!process.env.CI || process.env.npm_lifecycle_event === "lint",
-});
+  NEXT_PUBLIC_TORUS_RPC_URL: z.string().url(),
+  NEXT_PUBLIC_TORUS_CACHE_URL: z.string().url(),
+  NEXT_PUBLIC_NODE_ENV: NodeEnvSchema.default(() => process.env.NODE_ENV),
+})
+
+export const { EnvScript, env } = buildZodEnvScript(EnvSchema, {
+  skipValidation: !!process.env.CI || process.env.npm_lifecycle_event === "lint",
+})
