0b4bfea2881898e41f1c9db13094721b4e304ae3
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-16 11:31:58 -0300

    fix: type inference of `env()` output ; record of zod schemas instead of object zod schema for env; don't fill process.env;

--- packages/env-validation/src/EnvScript.tsx
+++ packages/env-validation/src/EnvScript.tsx
@@ -1,17 +1,19 @@
 /**
  * This ReactScript is used to validate and set the environment variables in runtime.
- * 
+ *
  * The envs should be accessed using the `env` function.
  * On the client side, the environment variables are set in the window.__ENV object.
  * On the server side, the environment variables are set in the process.env object.
- * 
+ *
  */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import type { z } from "zod";
-import { PublicEnvScript, env } from "next-runtime-env"
-import { unstable_noStore as noStore } from "next/cache";
-import type { NonceConfig } from "next-runtime-env/build/typings/nonce";
+
 import type { FC } from "react";
+
+import { env, PublicEnvScript } from "next-runtime-env";
+import type { NonceConfig } from "next-runtime-env/build/typings/nonce";
+import { unstable_noStore as noStore } from "next/cache";
+import type { ZodType, ZodTypeAny } from "zod";
+import { z } from "zod";
 
 interface EnvScriptProps {
   nonce?: string | NonceConfig;
@@ -19,7 +21,7 @@ interface EnvScriptProps {
 
 interface ZodEnvScriptProps extends EnvScriptProps {
   skipValidation?: boolean;
-  schema: z.ZodObject<any, any>;
+  schema: Record<string, ZodTypeAny>;
 }
 
 /**
@@ -29,55 +31,76 @@ interface ZodEnvScriptProps extends EnvScriptProps {
  * This component is disables Next.js' caching mechanism to ensure that the
  * environment variables are always up-to-date.
  */
-export const ZodEnvScript: FC<ZodEnvScriptProps> = ({ nonce, skipValidation, schema }) => {
+export const ZodEnvScript: FC<ZodEnvScriptProps> = ({
+  nonce,
+  skipValidation,
+  schema,
+}) => {
   noStore();
+
   if (!skipValidation) {
-    const envs = schema.parse(process.env);
-    // setting default values and transformations, if any
-    for (const [key, value] of Object.entries(envs)) {
-      // if zod schema has a default value, process.env[key] will be set to that value
-      process.env[key] = value as string;
+    const objSchema = z.object(schema);
+    const result = objSchema.safeParse(process.env);
+
+    if (!result.success) {
+      throw new Error(
+        `Failed to validate environment variables: ${result.error.message}`,
+      );
     }
+
+    // Note: zod can set default values, for our parsed envs, but they are not
+    // being set in the process.env object. The code below would do that.
+
+    // // Setting parsed and default values on process.env
+    // for (const [key, value] of Object.entries(envs)) {
+    //   process.env[key] = value as string;
+    // }
   }
 
-
-  return <PublicEnvScript nonce={nonce} />
+  return <PublicEnvScript nonce={nonce} />;
 };
 
-
 export interface BuildEnvProviderOptions {
+  /** Skip validation of environment variables */
   skipValidation?: boolean;
 }
 
 /**
- * This function builds 
- * - EnvScript: an script that uses Zod to validate and transform environment variables.
+ * This function builds
+ * - EnvScript: A script that sets the public environment variables in the
+ *   browser, using the Zod schemas to parse environment variables.
  * - env: a function that returns the value of an environment variable.
  *
- * @param schema Zod schema for the environment variables
- * @param opts 
+ * @param schema Zod schemas for the environment variables
  * @param opts.skipValidation Skip validation of environment variables
- * @returns \{ EnvScript: FC<EnvScriptProps>, env: (key: string) => T[keyof T] }
- * 
- * Usage:
- * Add the following to your `head` tag in your Layout component.
+ *
+ * Usage: Add the following to your `head` tag in your Layout component.
  * ```ts
  * <head>
  *   <EnvScript />
  * </head>
  * ```
- * 
+ *
  * Access the environment variables in your components using the `env` function.
  * ```ts
- * const a = env('NEXT_PUBLIC_FAVOURITE_COLOR')
+ * const a = env('NEXT_PUBLIC_FAVORITE_COLOR')
  * ```
  */
-export function buildZodEnvScript<Z extends z.ZodObject<any, any>, T = z.infer<Z>>(schema: Z, opts: BuildEnvProviderOptions): {
+export function buildZodEnvScript<S extends Record<string, ZodType<unknown>>>(
+  schema: S,
+  opts?: BuildEnvProviderOptions,
+): {
   EnvScript: FC<EnvScriptProps>;
-  env: (key: keyof z.infer<typeof schema>) => T[keyof T];
+  env: (key: string & keyof S) => z.infer<S[keyof S]>;
 } {
   return {
-    EnvScript: (props) => <ZodEnvScript schema={schema} skipValidation={opts?.skipValidation} {...props} />,
-    env: (key) => env(key as string) as T[keyof T]
-  }
+    EnvScript: (props) => (
+      <ZodEnvScript
+        schema={schema}
+        skipValidation={opts?.skipValidation}
+        {...props}
+      />
+    ),
+    env: <K extends keyof S>(key: K & string) => env(key) as z.infer<S[K]>,
+  };
 }
