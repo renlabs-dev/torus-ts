64f4cb88284ca0c58d9e065dc7a83bba6640e412
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-18 18:40:03 -0300

    feat: add IPFS URI and CID schemas

--- packages/utils/src/ipfs.ts
+++ packages/utils/src/ipfs.ts
@@ -1,6 +1,7 @@
 import { CID } from "multiformats/cid";
 import { z } from "zod";
 
+import { assert_error } from "./";
 import type { Result } from "./typing";
 
 export const URL_SCHEMA = z.string().trim().url();
@@ -8,6 +9,40 @@ export const URL_SCHEMA = z.string().trim().url();
 export interface CustomDataError {
   message: string;
 }
+
+const IPFS_URI_REGEX = /^ipfs:\/\/(\w+)$/;
+
+export const CID_SCHEMA = z.string().transform((cid, ctx) => {
+  let cidResult;
+  try {
+    cidResult = CID.parse(cid);
+  } catch (err) {
+    assert_error(err);
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `Invalid CID '${cid}', ${err}`,
+    });
+    return z.NEVER;
+  }
+  return cidResult;
+});
+
+export const IPFS_URI_SCHEMA = z
+  .string()
+  .url()
+  .transform((uri, ctx) => {
+    const match = IPFS_URI_REGEX.exec(uri);
+    const cidTxt = match?.[1];
+    if (!cidTxt) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Invalid IPFS URI '${uri}', does not match format 'ipfs://\\d+'`,
+      });
+      return z.NEVER;
+    }
+    return cidTxt;
+  })
+  .pipe(CID_SCHEMA);
 
 export function buildIpfsGatewayUrl(cid: CID): string {
   const cidStr = cid.toString();
@@ -21,6 +56,9 @@ const handleCleanPrefix = (uri: string, prefix: string): string => {
   return uri;
 };
 
+/**
+ * @deprecated Use IPFS_URI_SCHEMA instead.
+ */
 export function parseIpfsUri(uri: string): Result<CID, CustomDataError> {
   const ipfsPrefix = "ipfs://";
   const validated = URL_SCHEMA.safeParse(uri);
@@ -33,8 +71,9 @@ export function parseIpfsUri(uri: string): Result<CID, CustomDataError> {
 
     const cid = CID.parse(uri);
     return { Ok: cid };
-  } catch (e) {
-    const message = `Unable to parse IPFS URI '${uri}'`;
+  } catch (err) {
+    assert_error(err);
+    const message = `Unable to parse IPFS URI '${uri}', ${err}`;
     return { Err: { message } };
   }
 }
