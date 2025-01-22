e5004c3037d13cbe89c59b346fd91dd03ff672eb
Kelvin Steiner <me@steinerkelvin.dev>
2025-01-22 15:22:15 -0300

    feat: add NEXT_PUBLIC_TORUS_CHAIN_ENV env var to specify general chain related environment

--- apps/torus-governance/src/env.ts
+++ apps/torus-governance/src/env.ts
@@ -3,9 +3,13 @@ import { buildZodEnvScript } from "@torus-ts/env-validation";
 
 const AUTH_ORIGIN_DEFAULT = "governance.torus.network";
 
+const NodeEnvSchema = z
+  .enum(["development", "production", "test"])
+  .default("development");
 
-const NodeEnvSchema = z.enum(["development", "production", "test"]).default("development");
-if (process?.env) { // using Reflect to avoid inlining by Next  https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
+if (process?.env) {
+  // using Reflect to avoid inlining by Next
+  // https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
   Reflect.set(process.env, "NEXT_PUBLIC_NODE_ENV", process.env.NODE_ENV);
 }
 
@@ -21,7 +25,7 @@ export const envSchema = {
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
    */
   /** Origin URI used in the statement signed by the user to authenticate */
-  NEXT_PUBLIC_AUTH_ORIGIN: z.string().default(AUTH_ORIGIN_DEFAULT), // Origin URI used in the statement signed by the user to authenticate
+  NEXT_PUBLIC_AUTH_ORIGIN: z.string().default(AUTH_ORIGIN_DEFAULT),
   NEXT_PUBLIC_TORUS_RPC_URL: z.string().url(),
   NEXT_PUBLIC_TORUS_CACHE_URL: z.string().url(),
   NEXT_PUBLIC_NODE_ENV: NodeEnvSchema,
--- apps/torus-wallet/src/env.ts
+++ apps/torus-wallet/src/env.ts
@@ -1,21 +1,26 @@
 import { z } from "zod";
+
 import { buildZodEnvScript } from "@torus-ts/env-validation";
 
-const NodeEnvSchema = z.enum(["development", "production", "test"]).default("development");
-if (process?.env) { // using Reflect to avoid inlining by Next  https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
+const NodeEnvSchema = z
+  .enum(["development", "production", "test"])
+  .default("development");
+if (process?.env) {
+  // using Reflect to avoid inlining by Next  https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
   Reflect.set(process.env, "NEXT_PUBLIC_NODE_ENV", process.env.NODE_ENV);
 }
 
 // warning: DO NOT expose any sensitive data on the schema default values!
 export const envSchema = {
   NODE_ENV: NodeEnvSchema.default("development"),
-  /**
+  /*
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
    */
+  NEXT_PUBLIC_NODE_ENV: NodeEnvSchema,
+  NEXT_PUBLIC_TORUS_CHAIN_ENV: z.enum(["mainnet", "testnet"]),
   NEXT_PUBLIC_TORUS_RPC_URL: z.string().url(),
   NEXT_PUBLIC_TORUS_CACHE_URL: z.string().url(),
-  NEXT_PUBLIC_NODE_ENV: NodeEnvSchema,
 };
 
 export const { EnvScript, env } = buildZodEnvScript(envSchema, {
