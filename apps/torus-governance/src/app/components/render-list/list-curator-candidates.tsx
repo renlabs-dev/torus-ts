33e5dd4da0e805513efba3cd14d8c56c4f82d879
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-01-28 21:58:07 -0300

    refactor: remove CuratorNotAuthenticated component and integrate its logic into ListCuratorCandidates

--- apps/torus-governance/src/app/components/render-list/list-curator-candidates.tsx
+++ apps/torus-governance/src/app/components/render-list/list-curator-candidates.tsx
@@ -4,8 +4,8 @@ import { CardSkeleton } from "../card-skeleton";
 import { CuratorCandidatesList } from "../cadre/curator-candidates-list";
 import { ListContainer } from "./list-container";
 import { useGovernance } from "~/context/governance-provider";
-import { Card } from "@torus-ts/ui";
-import { CuratorNotAuthenticated } from "../cadre/curator-not-authenticated";
+import { Button, Card } from "@torus-ts/ui";
+import { CreateCadreCandidates } from "../agent-application/create-cadre-candidates";
 
 const ListCardsLoadingSkeleton = () => {
   return (
@@ -29,6 +29,8 @@ export const ListCuratorCandidates = () => {
     cadreCandidates,
     isUserAuthenticated,
     authenticateUser,
+    isUserCadre,
+    isUserCadreCandidate,
   } = useGovernance();
 
   const { data: cadreCandidatesList, isFetching: isFetchingCadreCandidates } =
@@ -46,8 +48,48 @@ export const ListCuratorCandidates = () => {
       </Card>
     );
 
-  if (!isUserAuthenticated)
-    return <CuratorNotAuthenticated authenticateUser={authenticateUser} />;
+  if (isUserCadre && !isUserAuthenticated)
+    return (
+      <Card className="px-4 py-8">
+        <div className="flex flex-col items-center justify-center gap-1">
+          <h1 className="text-center font-bold">You are not authenticated.</h1>
+          <p className="text-center">Please sign in to view this page.</p>
+          <Button
+            variant="default"
+            className="mt-4 px-4 py-2"
+            onClick={() => authenticateUser()}
+          >
+            Sign in
+          </Button>
+        </div>
+      </Card>
+    );
+
+  if (!isUserCadre && !isUserCadreCandidate)
+    return (
+      <Card className="py-8">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-center font-bold">
+            This content is only avaiable to Curator DAO Members.
+          </h1>
+          <CreateCadreCandidates />
+        </div>
+      </Card>
+    );
+
+  if (isUserCadreCandidate && !isUserCadre)
+    return (
+      <Card className="px-4 py-8">
+        <div className="flex flex-col items-center justify-center gap-1">
+          <h1 className="text-center font-bold">
+            Your candidacy is under vote.
+          </h1>
+          <p className="text-center">
+            Access will be granted once approved. Thank you for your patience.
+          </p>
+        </div>
+      </Card>
+    );
 
   if (!cadreCandidatesList && isFetchingCadreCandidates)
     return <ListCardsLoadingSkeleton />;
@@ -70,5 +112,6 @@ export const ListCuratorCandidates = () => {
     );
   });
 
-  return <ListContainer>{filteredCadreCandidates}</ListContainer>;
+  if (isUserCadre && isUserAuthenticated)
+    return <ListContainer>{filteredCadreCandidates}</ListContainer>;
 };
