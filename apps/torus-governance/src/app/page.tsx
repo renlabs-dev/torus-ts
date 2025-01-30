10f8196a17375f1b4ecf0e0db751e09f0ac35d7d
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-01-30 19:22:51 -0300

    fix: adjust margin in main layout for improved spacing

--- apps/torus-governance/src/app/page.tsx
+++ apps/torus-governance/src/app/page.tsx
@@ -7,7 +7,7 @@ import { SidebarLinks } from "./components/sidebar-links";
 
 export default function HomePage(): JSX.Element {
   return (
-    <main className="mb-12 flex w-full animate-fade-down flex-col gap-4 lg:flex-row">
+    <main className="mb-6 flex w-full animate-fade-down flex-col gap-4 lg:flex-row">
       <div className="flex w-full flex-col gap-4 lg:w-2/5 lg:max-w-[320px]">
         <div className="flex w-full gap-4">
           <SidebarLinks />
