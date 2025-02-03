dcdc9b827278a23daee79e0b66a0993c95d81837
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-02-03 17:28:35 -0300

    fix: remove unnecessary whitespace in cadre-vote-history.ts

--- packages/api/src/router/cadre/cadre-vote-history.ts
+++ packages/api/src/router/cadre/cadre-vote-history.ts
@@ -4,7 +4,7 @@ import "@torus-ts/db/schema";
 
 import { publicProcedure } from "../../trpc";
 import { cadreVoteHistory } from "@torus-ts/db/schema";
-import {  isNull } from "@torus-ts/db";
+import { isNull } from "@torus-ts/db";
 
 export const cadreVoteHistoryRouter = {
   // GET
