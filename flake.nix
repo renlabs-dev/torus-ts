180cd0942b35bc0239d658889eac22089b6a059c
Kelvin Steiner <me@steinerkelvin.dev>
2025-02-07 15:14:10 -0300

    chore: fix flake on MacOS (systemd dep); move from nixos-unstable to nixos-24.11
    `systemd` package is being used on LInux to serve USB to some npm package that the Hyperlane bridge depends on.
    Our other repos are using `nixos-24.11`.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -8,28 +8,27 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        nativeBuildInputs = [
-          # systemd provides libudev for `npm:usb` because of Solana adapter on Hyperlane
+        lib = pkgs.lib;
+        nativeBuildInputs = lib.optionals pkgs.stdenv.isLinux [
+          # `systemd` provides libudev for `npm:usb` because of Solana adapter on
+          # Hyperlane, we should enable it only on Linux
           pkgs.systemd
         ];
         buildInputs = [
+          # Node.js
           pkgs.nodejs_20
           pkgs.pnpm
         ];
         shellPkgs = [
-          # # Git LFS
-          # pkgs.git-lfs
           # Run project-specific commands
           pkgs.just
           # Run Github actions locally
           pkgs.act
         ];
-      in
-      {
+      in {
         devShell = pkgs.mkShell {
           inherit nativeBuildInputs buildInputs;
           packages = shellPkgs;
         };
-      }
-    );
+      });
 }
