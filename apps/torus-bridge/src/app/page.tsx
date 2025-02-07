897b3828c2b437a7f19db0856261582292c030b4
EdSDR <ed.sdr@outlook.com>
2025-02-07 18:53:11 -0300

    refactor: remove unnecessary blank line in HomePage component

--- apps/torus-bridge/src/app/page.tsx
+++ apps/torus-bridge/src/app/page.tsx
@@ -15,7 +15,6 @@ export default function HomePage(): JSX.Element {
         <SelectActionDialog />
         <GuideDialog />
       </div>
-
       <div className="flex w-full flex-col justify-around gap-6 lg:flex-row">
         <div className="flex w-full animate-fade flex-col gap-4 lg:w-4/12">
           <SidebarLinks />
