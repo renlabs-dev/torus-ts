cfcd435ef91532efecd1e24f10b671ed477f0ed0
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2025-02-07 20:58:07 -0300

    feat: add ListContainer component for handling overflow and smooth scrolling

---
+++ apps/torus-governance/src/app/components/render-list/container-list.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+
+export const ListContainer = (props: {
+  children: React.ReactNode;
+  smallesHeight?: number;
+  className?: string;
+}) => {
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      const contentHeight = contentRef.current.scrollHeight;
+      const maxAllowedHeight =
+        window.innerHeight - (props.smallesHeight ?? 280);
+      setIsOverflowing(contentHeight > maxAllowedHeight);
+      scrollTo({ top: 100, behavior: "smooth" });
+    }
+  }, [props.smallesHeight]);
+
+  return (
+    <div
+      ref={contentRef}
+      className={`max-h-[calc(100svh-280px)] animate-fade-down overflow-y-auto lg:max-h-[calc(100svh-220px)] ${props.className}`}
+    >
+      <div
+        className={`flex flex-col-reverse gap-4 ${isOverflowing ? "pr-1 md:pr-2" : ""}`}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+};
