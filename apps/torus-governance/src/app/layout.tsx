b7087dc534e2da5dcb2f5f9e480f739eaa181208
https://github.com/Sipher <vitor.chiarelli@gmail.com>
2025-03-15 12:10:18 -0300

    even more updates

--- apps/torus-governance/src/app/layout.tsx
+++ apps/torus-governance/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@torus-ts/ui/globals.css";
+import DiscordAuthProvider from "./providers/auth-provider";
 import { TorusProvider } from "@torus-ts/torus-provider";
 import { Container } from "@torus-ts/ui/components/container";
 import { Footer } from "@torus-ts/ui/components/footer";
@@ -38,9 +39,11 @@ export default function RootLayout({
       >
         <TRPCReactProvider>
           <GovernanceProvider>
-            <Container>{children}</Container>
-            <Footer torusChainEnv={env("NEXT_PUBLIC_TORUS_CHAIN_ENV")} />
-            <Toaster />
+            <DiscordAuthProvider>
+              <Container>{children}</Container>
+              <Footer />
+              <Toaster />
+            </DiscordAuthProvider>
           </GovernanceProvider>
         </TRPCReactProvider>
       </TorusProvider>
