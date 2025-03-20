45406b5823faff0cc5890f79a652bc1e849f86c5
DaviPtrs <davispetris@gmail.com>
2025-03-20 16:34:23 -0300

    test

--- apps/torus-governance/src/app/loader.tsx
+++ apps/torus-governance/src/app/loader.tsx
@@ -5,6 +5,7 @@ export default function Loader() {
     <div className="flex min-h-screen w-full items-center justify-center lg:h-auto">
       <h1 className="text-2xl text-white">Loading...</h1>
       <LoaderCircle className="ml-2 animate-spin" color="#FFF" width={20} />
+
     </div>
   );
 }
