362bd839a6b11bde6532a53111a0b3abdd8089ce
https://github.com/Sipher <vitor.chiarelli@gmail.com>
2025-03-21 13:56:45 -0300

    quickfix for the weekly the week display

--- apps/torus-allocator/src/hooks/use-weekly-usd.ts
+++ apps/torus-allocator/src/hooks/use-weekly-usd.ts
@@ -1,9 +1,12 @@
 import {
   useGetTorusPrice,
+  useRecyclingPercentage,
+  useTreasuryEmissionFee,
   // useBlockEmission,
   // useIncentivesRatio,
 } from "@torus-ts/query-provider/hooks";
 import { CONSTANTS } from "@torus-ts/subspace";
+import { useTorus } from "@torus-ts/torus-provider";
 import { useMemo } from "react";
 import { api as extAPI } from "~/trpc/react";
 
@@ -48,6 +51,14 @@ export function useWeeklyUsdCalculation(
     },
   );
 
+  const { api } = useTorus();
+
+  // Gets the information of Recycling Percentage
+  const { data: recyclingPercentage } = useRecyclingPercentage(api);
+
+  // Gets the information of Treasury Emission Fee
+  const { data: treasuryEmissionFee } = useTreasuryEmissionFee(api);
+
   // Loads all queries at once, and if any of them are wrong, the whole result is wrong
   const isLoading = isTorusPriceLoading || isComputedWeightLoading;
   const isError = isTorusPriceError || isComputedWeightError;
@@ -63,8 +74,18 @@ export function useWeeklyUsdCalculation(
     // Weight Factor is the penalty factor
     const weightPenaltyFactor = props.weightFactor ?? 1; // Default to 1 if not available
 
-    // Calculate weekly emission in NANOs and convert to tokens
-    const weeklyEmission = CONSTANTS.EMISSION.BLOCK_EMISSION * BLOCKS_PER_WEEK;
+    // Calculate weekly emission in REMs and convert to tokens
+    const fullWeeklyEmission =
+      CONSTANTS.EMISSION.BLOCK_EMISSION * BLOCKS_PER_WEEK;
+
+    // Converts the reciclyn rate to percentage
+    const percReciclyngRate = Number(recyclingPercentage) / 100;
+
+    // Converts the treasury emission fee to percentage
+    const percTreasuryEmissionFee = Number(treasuryEmissionFee) / 100;
+
+    const weeklyEmission =
+      fullWeeklyEmission * (1 - (percReciclyngRate + percTreasuryEmissionFee));
 
     // Computed Weight but the percentage
     const percComputedWeight = computedWeightedAgents.percComputedWeight;
@@ -80,6 +101,8 @@ export function useWeeklyUsdCalculation(
       (1 - percWeightPenaltyFactor)
     );
   }, [
+    recyclingPercentage,
+    treasuryEmissionFee,
     computedWeightedAgents?.percComputedWeight,
     computedWeightedAgents?.computedWeight,
     props.weightFactor,
