2264cf632bef57ee745bb0114dbca3ea5a0bfb24
EdSDR <ed.sdr@outlook.com>
2025-04-11 15:39:31 -0300

    fix: container size on dao expanded pages

--- apps/torus-governance/src/app/(expanded-pages)/agent-application/[id]/page.tsx
+++ apps/torus-governance/src/app/(expanded-pages)/agent-application/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@torus-ts/ui/components/button";
+import { Container } from "@torus-ts/ui/components/container";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { AgentApplicationExpandedView } from "./_components/agent-application-expanded-view";
@@ -15,24 +16,26 @@ export default async function AgentApplicationView({
   }
 
   return (
-    <div className="mx-auto flex w-full max-w-screen-xl flex-col pb-20">
-      <Button
-        asChild
-        variant="link"
-        className="mb-6 flex w-fit items-center gap-1.5 p-0"
-      >
-        <Link
-          href="/?view=agent-applications"
-          className="animate-fade-left flex items-center text-white transition duration-200"
+    <Container>
+      <div className="mx-auto flex w-full max-w-screen-xl flex-col pb-20">
+        <Button
+          asChild
+          variant="link"
+          className="mb-6 flex w-fit items-center gap-1.5 p-0"
         >
-          <ArrowLeft className="h-5 w-5" />
-          Go back to agents/modules list
-        </Link>
-      </Button>
+          <Link
+            href="/?view=agent-applications"
+            className="animate-fade-left flex items-center text-white transition duration-200"
+          >
+            <ArrowLeft className="h-5 w-5" />
+            Go back to agents/modules list
+          </Link>
+        </Button>
 
-      <div className="flex h-full w-full flex-col justify-between divide-gray-500 text-white lg:flex-row">
-        <AgentApplicationExpandedView paramId={Number(id)} />
+        <div className="flex h-full w-full flex-col justify-between divide-gray-500 text-white lg:flex-row">
+          <AgentApplicationExpandedView paramId={Number(id)} />
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
