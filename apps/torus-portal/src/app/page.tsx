de3afdb2ccc8665206fe05ca70857d684da0e7b7
EdSDR <ed.sdr@outlook.com>
2025-05-01 09:25:22 -0300

    feat: permission app initial repo config

---
+++ apps/torus-portal/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main
+      className="container mx-auto min-h-screen max-w-screen-lg w-full flex items-center
+        justify-center"
+    >
+      TODO
+    </main>
+  );
+}
