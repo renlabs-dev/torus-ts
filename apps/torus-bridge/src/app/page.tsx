66407b36073a3207778d0c2d63bb9d1623dd17b3
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-05-04 17:35:02 -0300

    chore: comment out WalletBalance component

--- apps/torus-bridge/src/app/page.tsx
+++ apps/torus-bridge/src/app/page.tsx
@@ -4,12 +4,15 @@ import { GuideDialog } from "./_components/guide-dialog";
 import { SelectActionDialog } from "./_components/select-action-dialog";
 import { SidebarLinks } from "./_components/shared/sidebar-links";
 import WalletActions from "./_components/shared/wallet-actions";
-import { WalletBalance } from "./_components/shared/wallet-balance";
+// import { WalletBalance } from "./_components/shared/wallet-balance";
 import { TransferDetails } from "./_components/transfer-details";
 
 export default function HomePage() {
   return (
-    <main className="mx-auto flex min-w-full flex-col items-start gap-3 text-white lg:mt-[calc(20vh-64px)]">
+    <main
+      className="mx-auto flex min-w-full flex-col items-start gap-3 text-white
+        lg:mt-[calc(20vh-64px)]"
+    >
       <TransferDetails />
       <div className="mb-4 mt-12 flex w-full flex-col gap-6 md:mt-0 md:flex-row">
         <SelectActionDialog />
@@ -18,7 +21,7 @@ export default function HomePage() {
       <div className="flex w-full flex-col justify-around gap-6 lg:flex-row">
         <div className="animate-fade flex w-full flex-col gap-4 lg:w-4/12">
           <SidebarLinks />
-          <WalletBalance />
+          {/* <WalletBalance /> */}
         </div>
         <div className="flex w-full flex-col gap-6">
           <WalletActions />
