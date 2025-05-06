8585e6cd961ba03a114641604498a9c9d0c48a95
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-05-06 13:02:20 -0300

    chore: comment out unused code and restore dialog in TransferDetails

--- apps/torus-bridge/src/app/_components/transfer-details/index.tsx
+++ apps/torus-bridge/src/app/_components/transfer-details/index.tsx
@@ -1,45 +1,47 @@
-import { useStore } from "~/utils/store";
+// import { useStore } from "~/utils/store";
 import type { TransferContext } from "~/utils/types";
-import { useEffect, useRef, useState } from "react";
+import {
+  // useEffect,
+  // useRef,
+  useState,
+} from "react";
 import { TransfersDetailsDialog } from "./_components/transfer-details-dialog";
 
 export function TransferDetails() {
-  const didMountRef = useRef(false);
+  // const didMountRef = useRef(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTransfer, setSelectedTransfer] = useState<
     TransferContext | undefined | null
   >(null);
 
-  const { transfers, transferLoading } = useStore((s) => ({
-    transfers: s.transfers,
-    resetTransfers: s.resetTransfers,
-    transferLoading: s.transferLoading,
-  }));
+  // const { transfers, transferLoading } = useStore((s) => ({
+  //   transfers: s.transfers,
+  //   resetTransfers: s.resetTransfers,
+  //   transferLoading: s.transferLoading,
+  // }));
 
-  useEffect(() => {
-    if (!didMountRef.current) {
-      didMountRef.current = true;
-    } else if (transferLoading) {
-      setSelectedTransfer(transfers[transfers.length - 1]);
-      setIsModalOpen(true);
-    }
-  }, [transfers, transferLoading]);
+  // useEffect(() => {
+  //   if (!didMountRef.current) {
+  //     didMountRef.current = true;
+  //   } else if (transferLoading) {
+  //     setSelectedTransfer(transfers[transfers.length - 1]);
+  //     setIsModalOpen(true);
+  //   }
+  // }, [transfers, transferLoading]);
 
-  // return (
-  //   <>
-  //     {selectedTransfer && (
-  //       <TransfersDetailsDialog
-  //         isOpen={isModalOpen}
-  //         onClose={() => {
-  //           setIsModalOpen(false);
-  //           setSelectedTransfer(null);
-  //         }}
-  //         transfer={selectedTransfer}
-  //       />
-  //     )}
-  //   </>
-  // );
-
-  return null;
+  return (
+    <>
+      {selectedTransfer && (
+        <TransfersDetailsDialog
+          isOpen={isModalOpen}
+          onClose={() => {
+            setIsModalOpen(false);
+            setSelectedTransfer(null);
+          }}
+          transfer={selectedTransfer}
+        />
+      )}
+    </>
+  );
 }
