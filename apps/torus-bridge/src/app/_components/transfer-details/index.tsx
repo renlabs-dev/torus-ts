f2aa32d9580bdbd9889d2463050fe669b4c77aad
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-05-06 13:16:27 -0300

    chore: enable transfer details modal functionality

--- apps/torus-bridge/src/app/_components/transfer-details/index.tsx
+++ apps/torus-bridge/src/app/_components/transfer-details/index.tsx
@@ -1,34 +1,32 @@
-// import { useStore } from "~/utils/store";
+"use client";
+
+import { useStore } from "~/utils/store";
 import type { TransferContext } from "~/utils/types";
-import {
-  // useEffect,
-  // useRef,
-  useState,
-} from "react";
+import { useEffect, useRef, useState } from "react";
 import { TransfersDetailsDialog } from "./_components/transfer-details-dialog";
 
 export function TransferDetails() {
-  // const didMountRef = useRef(false);
+  const didMountRef = useRef(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTransfer, setSelectedTransfer] = useState<
     TransferContext | undefined | null
   >(null);
 
-  // const { transfers, transferLoading } = useStore((s) => ({
-  //   transfers: s.transfers,
-  //   resetTransfers: s.resetTransfers,
-  //   transferLoading: s.transferLoading,
-  // }));
+  const { transfers, transferLoading } = useStore((s) => ({
+    transfers: s.transfers,
+    resetTransfers: s.resetTransfers,
+    transferLoading: s.transferLoading,
+  }));
 
-  // useEffect(() => {
-  //   if (!didMountRef.current) {
-  //     didMountRef.current = true;
-  //   } else if (transferLoading) {
-  //     setSelectedTransfer(transfers[transfers.length - 1]);
-  //     setIsModalOpen(true);
-  //   }
-  // }, [transfers, transferLoading]);
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+    } else if (transferLoading) {
+      setSelectedTransfer(transfers[transfers.length - 1]);
+      setIsModalOpen(true);
+    }
+  }, [transfers, transferLoading]);
 
   return (
     <>
