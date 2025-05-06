721d5c29602745a4c425e70be003a10a2dad01c5
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-05-06 12:11:17 -0300

    chore: comment out TransferDetails component in HomePage

--- apps/torus-bridge/src/app/page.tsx
+++ apps/torus-bridge/src/app/page.tsx
@@ -5,12 +5,15 @@ import { SelectActionDialog } from "./_components/select-action-dialog";
 import { SidebarLinks } from "./_components/shared/sidebar-links";
 import WalletActions from "./_components/shared/wallet-actions";
 import { WalletBalance } from "./_components/shared/wallet-balance";
-import { TransferDetails } from "./_components/transfer-details";
+// import { TransferDetails } from "./_components/transfer-details";
 
 export default function HomePage() {
   return (
-    <main className="mx-auto flex min-w-full flex-col items-start gap-3 text-white lg:mt-[calc(20vh-64px)]">
-      <TransferDetails />
+    <main
+      className="mx-auto flex min-w-full flex-col items-start gap-3 text-white
+        lg:mt-[calc(20vh-64px)]"
+    >
+      {/* <TransferDetails /> */}
       <div className="mb-4 mt-12 flex w-full flex-col gap-6 md:mt-0 md:flex-row">
         <SelectActionDialog />
         <GuideDialog />
