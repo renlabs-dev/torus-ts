1fd31d817b56cfa520fd51bfe998a8f5612790c4
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-05-06 22:36:20 -0300

    chore: add "use client" directive and clean up component code

--- apps/torus-bridge/src/app/_components/transfer-details/index.tsx
+++ apps/torus-bridge/src/app/_components/transfer-details/index.tsx
@@ -1,28 +1,20 @@
+"use client";
+
 import { useStore } from "~/utils/store";
 import type { TransferContext } from "~/utils/types";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { TransfersDetailsDialog } from "./_components/transfer-details-dialog";
 
 export function TransferDetails() {
-  const didMountRef = useRef(false);
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTransfer, setSelectedTransfer] = useState<
     TransferContext | undefined | null
   >(null);
 
-  const { transfers, transferLoading } = useStore((s) => ({
-    transfers: s.transfers,
-    resetTransfers: s.resetTransfers,
-    transferLoading: s.transferLoading,
-  }));
+  const transfers = useStore((s) => s.transfers);
+  const transferLoading = useStore((s) => s.transferLoading);
 
   useEffect(() => {
-    if (!didMountRef.current) {
-      didMountRef.current = true;
-      return;
-    }
-
     if (!transferLoading && transfers.length > 0) {
       setSelectedTransfer(transfers[transfers.length - 1]);
       setIsModalOpen(true);
@@ -44,5 +36,3 @@ export function TransferDetails() {
     </>
   );
 }
-
-export default TransferDetails;
