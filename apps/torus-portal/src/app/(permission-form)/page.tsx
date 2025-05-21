48aa2a3fc7354514b929386194b7dd71ea64667d
EdSDR <ed.sdr@outlook.com>
2025-05-21 16:37:21 +0200

    feat: add new Page component for PermissionFlow to encapsulate permission form functionality and improve structure

--- apps/torus-portal/src/app/(permission-form)/page.tsx
+++ apps/torus-portal/src/app/(permission-form)/page.tsx
@@ -1,4 +1,4 @@
-import PermissionFlow from "./_components/permission-form/permission-flow";
+import PermissionFlow from "../_components/permission-form/permission-flow";
 
 export default function Page() {
   return (
