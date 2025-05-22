cdd46618233566ca9c26ee96fd94aaf021fe6091
EdSDR <ed.sdr@outlook.com>
2025-05-22 11:23:58 +0200

    feat: react flow with constraint creation container

---
+++ apps/torus-portal/src/app/(permission-form)/page.tsx
@@ -0,0 +1,9 @@
+import PermissionFlow from "../_components/permission-form/permission-flow";
+
+export default function Page() {
+  return (
+    <main className="w-full h-screen p-0 m-0 overflow-hidden">
+      <PermissionFlow />
+    </main>
+  );
+}
