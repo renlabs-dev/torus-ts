a3896dbd2bf469a93e6f03719c81d139aa7d6398
EdSDR <ed.sdr@outlook.com>
2025-05-24 16:32:45 +0200

    feat: clean up unused parts of layout example

--- apps/torus-portal/src/app/(permission-flow)/_components/auto-layout-example/algorithms/index.ts
+++ apps/torus-portal/src/app/(permission-flow)/_components/auto-layout-example/algorithms/index.ts
@@ -1,25 +1,21 @@
-import { type Node, type Edge } from '@xyflow/react';
+import type { Node, Edge } from "@xyflow/react";
 
-import dagre from './dagre';
-import d3Hierarchy from './d3-hierarchy';
-import elk from './elk';
+import d3Hierarchy from "./d3-hierarchy";
 
 // the layout direction (T = top, R = right, B = bottom, L = left, TB = top to bottom, ...)
-export type Direction = 'TB' | 'LR' | 'RL' | 'BT';
+export type Direction = "TB" | "LR" | "RL" | "BT";
 
-export type LayoutAlgorithmOptions = {
+export interface LayoutAlgorithmOptions {
   direction: Direction;
   spacing: [number, number];
-};
+}
 
 export type LayoutAlgorithm = (
   nodes: Node[],
   edges: Edge[],
-  options: LayoutAlgorithmOptions
+  options: LayoutAlgorithmOptions,
 ) => Promise<{ nodes: Node[]; edges: Edge[] }>;
 
 export default {
-  dagre,
-  'd3-hierarchy': d3Hierarchy,
-  elk,
+  "d3-hierarchy": d3Hierarchy,
 };
