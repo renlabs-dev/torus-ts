8b8850f463892f830e33d45608a2b545b605c830
EdSDR <ed.sdr@outlook.com>
2025-05-29 21:30:30 +0200

    fix: inverted shit

--- packages/api/src/router/permission/permission.ts
+++ packages/api/src/router/permission/permission.ts
@@ -1,13 +1,13 @@
 import { and, isNull } from "@torus-ts/db";
-import { permissionDetailsSchema } from "@torus-ts/db/schema";
+import { permissionSchema } from "@torus-ts/db/schema";
 import type { TRPCRouterRecord } from "@trpc/server";
 import { publicProcedure } from "../../trpc";
 
 export const permissionRouter = {
   // GET
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.permissionDetailsSchema.findMany({
-      where: and(isNull(permissionDetailsSchema.deletedAt)),
+    return ctx.db.query.permissionSchema.findMany({
+      where: and(isNull(permissionSchema.deletedAt)),
     });
   }),
 } satisfies TRPCRouterRecord;
