5977aaf5c94f49220196a1b3f6f4f11bcbb6e87a
EdSDR <ed.sdr@outlook.com>
2025-05-29 17:54:16 +0200

    feat: convert trpc endpoint to a authenticatedProcedure

--- apps/torus-portal/src/trpc/react.tsx
+++ apps/torus-portal/src/trpc/react.tsx
@@ -2,6 +2,8 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppRouter } from "@torus-ts/api";
+import { createAuthLink, makeAuthenticateUserFn } from "@torus-ts/api/client";
+import { useTorus } from "@torus-ts/torus-provider";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { env } from "~/env";
@@ -31,8 +33,22 @@ export const api = createTRPCReact<AppRouter>();
 export function TRPCReactProvider({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
+  const { signHex } = useTorus();
+
+  const getStoredAuthorization = () => localStorage.getItem("authorization");
+  const setStoredAuthorization = (authorization: string) =>
+    localStorage.setItem("authorization", authorization);
+
+  const authenticateUser = makeAuthenticateUserFn(
+    getBaseUrl(),
+    env("NEXT_PUBLIC_AUTH_ORIGIN"),
+    setStoredAuthorization,
+    signHex,
+  );
+
   const trpcClient = api.createClient({
     links: [
+      createAuthLink(authenticateUser, getStoredAuthorization),
       loggerLink({
         enabled: (op) =>
           env("NEXT_PUBLIC_NODE_ENV") === "development" ||
@@ -43,6 +59,10 @@ export function TRPCReactProvider({ children }: { children: React.ReactNode }) {
         headers() {
           const headers: Record<string, string> = {};
           headers["x-trpc-source"] = "nextjs-react";
+          const authorization = localStorage.getItem("authorization");
+          if (authorization) {
+            headers.authorization = authorization;
+          }
           return headers;
         },
         transformer: SuperJSON,
@@ -61,5 +81,5 @@ export function TRPCReactProvider({ children }: { children: React.ReactNode }) {
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return window.location.origin;
-  return `http://localhost:3004}`;
+  return `http://localhost:3004`;
 };
