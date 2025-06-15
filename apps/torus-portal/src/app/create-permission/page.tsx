eefbae04971d4f725355c8c27f3f87f97ecae5d1
EdSDR <ed.sdr@outlook.com>
2025-06-15 14:47:27 +0200

    feat: fix form scroll

--- apps/torus-portal/src/app/create-permission/page.tsx
+++ apps/torus-portal/src/app/create-permission/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import PortalNavigationTabs from "../_components/portal-navigation-tabs";
 import GrantEmissionPermissionForm from "./_components/grant-emission-permission-form";
 import { PermissionSuccessDialog } from "./_components/permission-success-dialog";
+import { ScrollArea } from "@torus-ts/ui/components/scroll-area";
 import Image from "next/image";
 
 export default function Page() {
@@ -17,16 +18,20 @@ export default function Page() {
         <Image
           width={100}
           height={100}
-          src="/form-bg-signal.svg"
+          src="/form-bg-permission.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
 
-      <div className="flex flex-1 items-center py-16 justify-center bg-card animate-fade-down">
-        <GrantEmissionPermissionForm
-          onSuccess={() => setIsSuccessDialogOpen(true)}
-        />
+      <div className="flex flex-1 bg-card animate-fade-down">
+        <ScrollArea className="flex-1 h-svh">
+          <div className="flex items-center justify-center pt-16">
+            <GrantEmissionPermissionForm
+              onSuccess={() => setIsSuccessDialogOpen(true)}
+            />
+          </div>
+        </ScrollArea>
       </div>
 
       <PermissionSuccessDialog
