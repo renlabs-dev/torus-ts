bd845d6127b2854e7af9a22eaeac838c152dd826
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-06-19 11:20:52 -0300

    fix: updates isPromise to a function declaration
    Simplifies the `isPromise` utility by changing it from a constant arrow function to a standard function declaration.
    This improves readability without altering functionality.

--- apps/torus-bridge/src/utils/helpers.ts
+++ apps/torus-bridge/src/utils/helpers.ts
@@ -1,7 +1,8 @@
-export const isPromise = (value: unknown): value is Promise<unknown> =>
-  Boolean(
+export function isPromise(value: unknown): value is Promise<unknown> {
+  return Boolean(
     value &&
       typeof value === "object" &&
       "then" in value &&
       typeof value.then === "function",
   );
+}
