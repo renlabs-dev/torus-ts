6ed5038ce8be984910b4e56e17d15de6a1363c42
EdSDR <ed.sdr@outlook.com>
2025-06-21 09:37:44 +0200

    chore: change portal folter structure to use pages foldering

--- apps/torus-portal/src/app/(pages)/create-signal/page.tsx
+++ apps/torus-portal/src/app/(pages)/create-signal/page.tsx
@@ -1,9 +1,9 @@
-import PortalFormLayout from "../_components/portal-form-layout";
+import PortalFormLayout from "~/app/_components/portal-form-layout";
 import CreateSignalForm from "./_components/create-signal-form";
 
 export default function Page() {
   return (
-    <PortalFormLayout 
+    <PortalFormLayout
       imageSrc="/form-bg-signal.svg"
       imageAlt="Abstract decorative background illustrating signal creation"
     >
