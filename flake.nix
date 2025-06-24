2ea55d495f57fc118a72a045e1e3ef0b6f90ab94
Kelvin Steiner <me@steinerkelvin.dev>
2025-06-24 09:35:25 +0200

    chore: bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -25,7 +25,8 @@
           # Run Github actions locally
           pkgs.act
         ];
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
           inherit nativeBuildInputs buildInputs;
           packages = shellPkgs;
