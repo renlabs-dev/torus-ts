a3e8c4ea35bf8a5d53f7b4c17ab66ed00dc87f13
PsicoThePato <jmomcontato@gmail.com>
2025-07-07 09:39:03 -0300

    format

--- packages/torus-sdk-ts/src/agent/agent.ts
+++ packages/torus-sdk-ts/src/agent/agent.ts
@@ -390,7 +390,7 @@ export class AgentServer {
                 });
                 console.log(
                   `Cached namespace permission ${permissionId} for grantee ${permission.grantee} with paths:`,
-                  namespaceScope.paths.map(path => path.join(".")),
+                  namespaceScope.paths.map((path) => path.join(".")),
                 );
               }
             },
