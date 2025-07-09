6bc0830605fc78f74ca5786d37960285e2187681
EdSDR <ed.sdr@outlook.com>
2025-07-09 16:26:24 -0300

    feat: move sidebar to main layout

---
+++ apps/torus-portal/src/app/_components/sidebar/sidebar-container.tsx
@@ -0,0 +1,59 @@
+import type { ReactNode } from "react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@torus-ts/ui/components/breadcrumb";
+import { Separator } from "@torus-ts/ui/components/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@torus-ts/ui/components/sidebar";
+
+import { PortalWalletDropdown } from "~/app/_components/portal-wallet-dropdown";
+import { AppSidebar } from "~/app/_components/sidebar/app-sidebar";
+
+export default function SidebarContainer({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 justify-between">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="flex items-center gap-2 px-4">
+            <PortalWalletDropdown />
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
