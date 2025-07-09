d0a8abd660a274e587a1eb5b076250c9b90d97d0
EdSDR <ed.sdr@outlook.com>
2025-07-09 12:21:13 -0300

    fix: sidebar linting

--- apps/torus-portal/src/app/_components/sidebar/nav-user.tsx
+++ apps/torus-portal/src/app/_components/sidebar/nav-user.tsx
@@ -44,9 +44,11 @@ export function NavUser({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              className="data-[state=open]:bg-sidebar-accent
+                data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img src={user.avatar} alt={user.name} />
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
@@ -65,6 +67,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img src={user.avatar} alt={user.name} />
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
