6f668c889f525e17c4571c340477c8445c91ef3c
EdSDR <ed.sdr@outlook.com>
2025-07-10 16:13:07 -0300

    feat: switch seach input to command

--- apps/torus-portal/src/app/_components/sidebar/sidebar-container.tsx
+++ apps/torus-portal/src/app/_components/sidebar/sidebar-container.tsx
@@ -28,7 +28,7 @@ export default function SidebarContainer({
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 justify-between">
-          <div className="flex items-center gap-2 px-4 z-[100]">
+          <div className="flex items-center gap-2 px-4 z-50">
             <SidebarTrigger className="-ml-1" />
             <Separator
               orientation="vertical"
