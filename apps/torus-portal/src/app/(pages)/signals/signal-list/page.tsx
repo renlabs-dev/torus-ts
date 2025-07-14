7a85533be4455674dcbe52b406b71adc7b288122
EdSDR <ed.sdr@outlook.com>
2025-07-14 00:05:59 -0300

    wip: demand signals page

--- apps/torus-portal/src/app/(pages)/signals/signal-list/page.tsx
+++ apps/torus-portal/src/app/(pages)/signals/signal-list/page.tsx
@@ -5,9 +5,7 @@ import AllSignalsView from "./_components/all-signals-view";
 export default function Page() {
   return (
     <PortalFormContainer>
-      <div className="w-full max-w-4xl mx-auto px-4 pt-6 md:pt-0">
-        <AllSignalsView />
-      </div>
+      <AllSignalsView />
     </PortalFormContainer>
   );
 }
