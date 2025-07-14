bd001a7547c2b3414857ae148aa264a161246fe8
EdSDR <ed.sdr@outlook.com>
2025-07-14 14:58:09 -0300

    feat: update sidebar link and remove unused register agent form

--- apps/torus-portal/src/app/(pages)/permissions/edit-permission/page.tsx
+++ apps/torus-portal/src/app/(pages)/permissions/edit-permission/page.tsx
@@ -1,11 +1,9 @@
 import PortalFormContainer from "~/app/_components/portal-form-container";
 
-import RegisterAgentForm from "./_components/register-agent-form";
-
-export default function Page() {
+export default function EditPermissionFormPage() {
   return (
     <PortalFormContainer>
-      <RegisterAgentForm />
+      <EditPermissionForm />
     </PortalFormContainer>
   );
 }
--- apps/torus-portal/src/app/(pages)/signals/signal-list/page.tsx
+++ apps/torus-portal/src/app/(pages)/signals/signal-list/page.tsx
@@ -2,7 +2,7 @@ import PortalFormContainer from "~/app/_components/portal-form-container";
 
 import AllSignalsView from "./_components/all-signals-view";
 
-export default function Page() {
+export default function AllSignalsViewPage() {
   return (
     <PortalFormContainer>
       <AllSignalsView />
