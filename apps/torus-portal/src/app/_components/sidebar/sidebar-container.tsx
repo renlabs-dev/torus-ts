bf77e0cab3af27a3a60dd67e4560e6bec844647f
EdSDR <ed.sdr@outlook.com>
2025-07-15 02:16:22 -0300

    feat: sidebar breadcrumb

--- apps/torus-portal/src/app/_components/sidebar/sidebar-container.tsx
+++ apps/torus-portal/src/app/_components/sidebar/sidebar-container.tsx
@@ -1,13 +1,5 @@
 import type { ReactNode } from "react";
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@torus-ts/ui/components/breadcrumb";
 import { Separator } from "@torus-ts/ui/components/separator";
 import {
   SidebarInset,
@@ -17,6 +9,7 @@ import {
 
 import { PortalWalletDropdown } from "~/app/_components/portal-wallet-dropdown";
 import { AppSidebar } from "~/app/_components/sidebar/app-sidebar";
+import { SidebarBreadcrumb } from "~/app/_components/sidebar/sidebar-breadcrumb";
 
 export default function SidebarContainer({
   children,
@@ -37,19 +30,9 @@ export default function SidebarContainer({
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
             />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">Overview</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Hypergraph</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <SidebarBreadcrumb />
           </div>
-          <div className="flex items-center gap-2 px-4">
+          <div className="flex items-center gap-2 px-4 w-full sm:w-fit">
             <PortalWalletDropdown />
           </div>
         </header>
