aed860011d5e1a19c21e2e01b990ed334b13cee7
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-07-18 14:33:09 -0300

    feat: extracts explorer link generation to a function
    Moves the explorer link generation to its own function for
    better code organization and reusability.
    
    Removes the toast content handler component.

--- packages/torus-provider/src/_components/toast-content-handler.tsx
+++ packages/torus-provider/src/_components/toast-content-handler.tsx
@@ -1,20 +1,3 @@
-import * as React from "react";
-import type { CSSProperties } from "react";
-
-const divStyle: CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  gap: "0.5rem",
-  color: "hsl(240 5% 64.9%)",
-};
-
-const linkStyle: CSSProperties = {
-  color: "white",
-  textDecoration: "underline",
-  fontSize: "0.875rem",
-  cursor: "pointer",
-};
-
 export const getExplorerLink = ({
   wsEndpoint,
   hash,
@@ -22,53 +5,3 @@ export const getExplorerLink = ({
   wsEndpoint: string;
   hash: string;
 }) => `https://polkadot.js.org/apps/?rpc=${wsEndpoint}#/explorer/query/${hash}`;
-
-export const renderWaitingForValidation = (
-  hash: string,
-  wsEndpoint: string,
-) => (
-  <div style={divStyle}>
-    <p>Validating transaction in block.</p>
-    <a
-      style={linkStyle}
-      target="_blank"
-      href={getExplorerLink({ wsEndpoint, hash })}
-    >
-      View on block explorer
-    </a>
-  </div>
-);
-
-export const renderSuccessfulyFinalized = (
-  transactionType: string,
-  hash: string,
-  wsEndpoint: string,
-) => (
-  <div style={divStyle}>
-    <p>{transactionType} completed successfully</p>
-    <a
-      style={linkStyle}
-      target="_blank"
-      href={getExplorerLink({ wsEndpoint, hash })}
-    >
-      View on block explorer
-    </a>
-  </div>
-);
-
-export const renderFinalizedWithError = (
-  msg: string,
-  hash: string,
-  wsEndpoint: string,
-) => (
-  <div style={divStyle}>
-    <p>{msg}</p>
-    <a
-      style={linkStyle}
-      target="_blank"
-      href={getExplorerLink({ wsEndpoint, hash })}
-    >
-      View on block explorer
-    </a>
-  </div>
-);
