f7a125d3a5131c71dc568967ef6e3a8e6df5758f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-07-18 19:37:24 -0300

    feat: implements transaction sending functionality
    Adds a component to handle sending transactions, including metadata handling, signing, and broadcasting to the network.
    It also includes error handling, toast notifications for user feedback, and integration with the Polkadot API.
    Removes the toast content handler as it is no longer needed.

--- packages/torus-provider/src/_components/toast-content-handler.tsx
+++
@@ -1,7 +0,0 @@
-export const getExplorerLink = ({
-  wsEndpoint,
-  hash,
-}: {
-  wsEndpoint: string;
-  hash: string;
-}) => `https://polkadot.js.org/apps/?rpc=${wsEndpoint}#/explorer/query/${hash}`;
