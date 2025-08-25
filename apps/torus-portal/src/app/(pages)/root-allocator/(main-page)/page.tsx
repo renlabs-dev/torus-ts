4731b2d4a607949c37deff38a3864011ce9f163a
EdSDR <ed.sdr@outlook.com>
2025-08-25 13:08:02 -0300

    feat: working infinity scroll on allocator

--- apps/torus-portal/src/app/(pages)/root-allocator/(main-page)/page.tsx
+++ apps/torus-portal/src/app/(pages)/root-allocator/(main-page)/page.tsx
@@ -1,23 +1,19 @@
 import { parseSearchParams } from "~/utils/parse-search-params";
 
-import { AgentList } from "../_components/agent-list";
+import { InfiniteAgentList } from "../_components/infinite-agent-list";
 import { PageLayout } from "../_components/page-layout";
 
 export default async function Page(props: {
   searchParams: Promise<{
-    page?: string;
     search?: string;
   }>;
 }) {
   const searchParams = await props.searchParams;
-  const { page, search } = parseSearchParams(searchParams);
+  const { search } = parseSearchParams(searchParams);
 
   return (
     <PageLayout search={search}>
-      <AgentList 
-        page={page} 
-        search={search}
-      />
+      <InfiniteAgentList search={search} />
     </PageLayout>
   );
 }
