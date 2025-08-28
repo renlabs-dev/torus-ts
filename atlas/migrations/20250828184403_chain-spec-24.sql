19f6d5d050b8f9fb3f3f8d087793ae742b3af68f
PsicoThePato <jmomcontato@gmail.com>
2025-08-28 16:32:52 -0300

    fix migiations

--- atlas/migrations/20250828184403_chain-spec-24.sql
+++ atlas/migrations/20250828184403_chain-spec-24.sql
@@ -1,5 +1,21 @@
 -- Modify "emission_permissions" table
-ALTER TABLE "public"."emission_permissions" ADD COLUMN "weight_setter" character varying(256)[] NOT NULL, ADD COLUMN "recipient_manager" character varying(256)[] NOT NULL;
+-- Add columns as nullable first
+ALTER TABLE "public"."emission_permissions" 
+ADD COLUMN "weight_setter" character varying(256)[],
+ADD COLUMN "recipient_manager" character varying(256)[];
+
+-- Populate with data from permissions.grantee_account_id
+UPDATE "public"."emission_permissions" 
+SET "weight_setter" = ARRAY[p.grantee_account_id],
+    "recipient_manager" = ARRAY[p.grantee_account_id]
+FROM "public"."permissions" p
+WHERE "public"."emission_permissions".permission_id = p.permission_id
+AND p.grantee_account_id IS NOT NULL;
+
+-- Make them NOT NULL after populating
+ALTER TABLE "public"."emission_permissions" 
+ALTER COLUMN "weight_setter" SET NOT NULL,
+ALTER COLUMN "recipient_manager" SET NOT NULL;
 -- Modify "namespace_permissions" table
 ALTER TABLE "public"."namespace_permissions" ADD COLUMN "recipient" character varying(256) NOT NULL;
 -- Modify "permissions" table
