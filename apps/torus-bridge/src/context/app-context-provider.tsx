d0778a68ad885ee1fd8009ad09762cec29791809
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-09-03 12:19:05 -0300

    feat: implement ProviderRenderer component for nested provider rendering in torus-bridge app

--- apps/torus-bridge/src/context/app-context-provider.tsx
+++ apps/torus-bridge/src/context/app-context-provider.tsx
@@ -6,10 +6,12 @@ import { TorusProvider } from "@torus-ts/torus-provider";
 import { Container } from "@torus-ts/ui/components/container";
 import { Footer } from "@torus-ts/ui/components/footer";
 import { Toaster } from "@torus-ts/ui/components/toaster";
+import { ProviderRenderer } from "~/app/_components/provider-renderer";
 import { WalletHeader } from "~/app/_components/shared/wallet-header";
 import { CosmosWalletProvider } from "~/context/cosmos-wallet-provider";
 import { EvmWalletProvider } from "~/context/evm-wallet-provider";
 import { SolanaWalletProvider } from "~/context/solana-wallet-provider";
+import { StarknetWalletProvider } from "~/context/starknet-wallet-provider";
 import { WarpContextInitGateProvider } from "~/context/warp-context-init-gate-provider";
 import { env } from "~/env";
 
@@ -24,24 +26,25 @@ export function AppContextProvider({
   }
 
   return (
-    <ReactQueryProvider>
+    <ProviderRenderer
+      providers={[
+        ReactQueryProvider,
+        WarpContextInitGateProvider,
+        EvmWalletProvider,
+        SolanaWalletProvider,
+        StarknetWalletProvider,
+        CosmosWalletProvider,
+      ]}
+    >
       <TorusProvider
         wsEndpoint={env("NEXT_PUBLIC_TORUS_RPC_URL")}
         torusCacheUrl={env("NEXT_PUBLIC_TORUS_CACHE_URL")}
       >
-        <WarpContextInitGateProvider>
-          <EvmWalletProvider>
-            <SolanaWalletProvider>
-              <CosmosWalletProvider>
-                <WalletHeader />
-                <Container>{children}</Container>
-                <Toaster />
-                <Footer torusChainEnv={env("NEXT_PUBLIC_TORUS_CHAIN_ENV")} />
-              </CosmosWalletProvider>
-            </SolanaWalletProvider>
-          </EvmWalletProvider>
-        </WarpContextInitGateProvider>
+        <WalletHeader />
+        <Container>{children}</Container>
+        <Toaster />
+        <Footer torusChainEnv={env("NEXT_PUBLIC_TORUS_CHAIN_ENV")} />
       </TorusProvider>
-    </ReactQueryProvider>
+    </ProviderRenderer>
   );
 }
