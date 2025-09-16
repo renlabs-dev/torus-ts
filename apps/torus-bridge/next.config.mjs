a77c2afd396ba8b3b29853225fa96a35dc7f7b47
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-09-16 15:54:02 -0300

    chore: remove unnecessary environment variable imports from Next.js config
    - Deleted commented-out code for importing environment files in the Next.js configuration of both the torus-bridge and torus-wallet applications.
    - This cleanup enhances code readability and reduces clutter in the configuration files.

--- apps/torus-bridge/next.config.mjs
+++ apps/torus-bridge/next.config.mjs
@@ -1,9 +1,5 @@
 import { fileURLToPath } from "url";
 
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
-// WARNING: ONLY NEEDED IF NEXT_PUBLIC_* VARIABLES ARE USED IN THE APP DIRECTLY
-// createJiti(fileURLToPath(import.meta.url))("./src/env");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
--- apps/torus-wallet/next.config.mjs
+++ apps/torus-wallet/next.config.mjs
@@ -1,7 +1,3 @@
-// Import env files to validate at build time. Use jiti so we can load .ts files in here.
-// WARNING: ONLY NEEDED IF NEXT_PUBLIC_* VARIABLES ARE USED IN THE APP DIRECTLY
-// createJiti(fileURLToPath(import.meta.url))("./src/env");
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
