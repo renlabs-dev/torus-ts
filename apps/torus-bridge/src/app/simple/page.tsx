d029b4aa0329abd0f92b1a382ae52b353642a51c
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2025-09-25 21:49:48 -0300

    refactor: remove TransferDetails component from Simple Bridge page
    - Deleted the TransferDetails component from the Simple Bridge page to streamline the UI.
    - This change enhances the overall clarity and performance of the Simple Bridge application.

--- apps/torus-bridge/src/app/simple/page.tsx
+++ apps/torus-bridge/src/app/simple/page.tsx
@@ -1,7 +1,6 @@
 import { SidebarLinks } from "../_components/shared/sidebar-links";
 import { WalletBalance } from "../_components/shared/wallet-balance";
 import { SimpleBridge } from "../_components/simple-bridge";
-import { TransferDetails } from "../_components/transfer-details";
 
 export function generateMetadata() {
   return {
@@ -14,7 +13,6 @@ export function generateMetadata() {
 export default function SimpleBridgePage() {
   return (
     <main className="mx-auto flex min-w-full flex-col items-start gap-3 text-white lg:mt-[calc(20vh-64px)]">
-      <TransferDetails />
       <div className="flex w-full flex-col justify-around gap-6 lg:flex-row">
         <div className="animate-fade flex w-full flex-col gap-4 lg:w-4/12">
           <SidebarLinks />
