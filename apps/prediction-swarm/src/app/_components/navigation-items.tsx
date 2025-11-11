b1b7d295dbc503ba55a33bdb3876853f2a382271
EdSDR <ed.sdr@outlook.com>
2025-11-11 14:14:34 -0300

    feat: search prophet store

--- apps/prediction-swarm/src/app/_components/navigation-items.tsx
+++ apps/prediction-swarm/src/app/_components/navigation-items.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import { env } from "~/env";
-import { WalletDropdown } from "./wallet-dropdown";
+import { useSearchStore } from "~/store/search-store";
+import { Search } from "lucide-react";
 import { NavigationMenuDropdown } from "./navigation-menu-dropdown";
+import { WalletDropdown } from "./wallet-dropdown";
 
 export function NavigationItems() {
+  const openSearch = useSearchStore((state) => state.open);
+
   return (
     <div className="flex items-center gap-2">
+      <button
+        onClick={openSearch}
+        className="bg-background/80 border-border hover:bg-background/40 flex h-8 w-8 items-center justify-center border text-white/80 transition duration-200"
+      >
+        <Search className="!h-4 !w-4" />
+      </button>
       <NavigationMenuDropdown />
       <WalletDropdown
         variant="icon"
