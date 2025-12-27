bcdf38f7b843777cc8dcd903c6f386a90213c335
EdSDR <ed.sdr@outlook.com>
2025-12-27 18:40:45 -0300

    feat: submit prospect

--- apps/apostle-swarm/src/app/page.tsx
+++ apps/apostle-swarm/src/app/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import type { SS58Address } from "@torus-network/sdk/types";
-import { formatToken } from "@torus-network/torus-utils/torus";
-import { useFreeBalance } from "@torus-ts/query-provider/hooks";
-import { useTorus } from "@torus-ts/torus-provider";
+import { SubmitProspectDialog } from "./_components/submit-prospect-dialog";
 
 export default function Page() {
-  const { api, selectedAccount } = useTorus();
-
-  // @functor-flow -> example of using a function that comes from the sdk `queryFreeBalance()`
-  // but its adapted to react-query through the `query-provider` package as `useFreeBalance()`
-  const accountFreeBalance = useFreeBalance(
-    api,
-    selectedAccount?.address as SS58Address,
-  );
-
   return (
-    <main className="container mx-auto flex min-h-screen w-full max-w-screen-lg items-center justify-center">
-      Free balance:{" "}
-      {accountFreeBalance.data
-        ? formatToken(accountFreeBalance.data) + " TORURURURUS"
-        : "Connect wallet r3trd"}
+    <main className="container mx-auto flex min-h-screen w-full max-w-screen-lg flex-col items-center justify-center gap-6">
+      <h1 className="text-2xl font-bold">Apostle Swarm</h1>
+      <SubmitProspectDialog />
     </main>
   );
 }
