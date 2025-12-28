8a8d210450d0795cf7ec5c930fe992c2e447da20
EdSDR <ed.sdr@outlook.com>
2025-12-28 13:19:18 -0300

    feat: add user role badge and update submit prospect dialog logic based on apostle status

--- apps/apostle-swarm/src/app/_components/prophet-finder-header.tsx
+++ apps/apostle-swarm/src/app/_components/prophet-finder-header.tsx
@@ -6,9 +6,10 @@ import {
   useFreeBalance,
 } from "@torus-ts/query-provider/hooks";
 import { useTorus } from "@torus-ts/torus-provider";
-import { Header } from "@torus-ts/ui/components/header";
 import { WalletDropdown } from "@torus-ts/ui/components/wallet-dropdown/wallet-dropdown";
+import { cn } from "@torus-ts/ui/lib/utils";
 import { env } from "~/env";
+import Link from "next/link";
 
 export function ProphetFinderHeader({
   torusCacheUrl,
@@ -31,23 +32,28 @@ export function ProphetFinderHeader({
   const stakeOut = useCachedStakeOut(torusCacheUrl);
 
   return (
-    <Header
-      className="!border-none !bg-transparent"
-      appName="Torus Prophet Finder"
-      wallet={
-        <WalletDropdown
-          className="z-50"
-          balance={accountFreeBalance.data}
-          stakeOut={stakeOut.data}
-          accounts={accounts}
-          isInitialized={isInitialized}
-          selectedAccount={selectedAccount}
-          handleLogout={handleLogout}
-          handleGetWallets={handleGetWallets}
-          handleSelectWallet={handleSelectWallet}
-          torusChainEnv={env("NEXT_PUBLIC_TORUS_CHAIN_ENV")}
-        />
-      }
-    />
+    <header
+      className={cn(
+        "animate-fade-down border-border fixed z-[70] flex w-full items-center justify-between py-1 pl-4 pr-2",
+      )}
+    >
+      <Link className={cn("flex gap-3 py-1.5 hover:cursor-pointer")} href="/">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/logo.svg" alt="Logo" className="h-7 w-7 object-contain" />
+      </Link>
+
+      <WalletDropdown
+        className="z-50"
+        balance={accountFreeBalance.data}
+        stakeOut={stakeOut.data}
+        accounts={accounts}
+        isInitialized={isInitialized}
+        selectedAccount={selectedAccount}
+        handleLogout={handleLogout}
+        handleGetWallets={handleGetWallets}
+        handleSelectWallet={handleSelectWallet}
+        torusChainEnv={env("NEXT_PUBLIC_TORUS_CHAIN_ENV")}
+      />
+    </header>
   );
 }
